fd8e3d5bcce04e6b0811004214fdadc52c3b8d22
Mathieu Chartier <mathieuc@google.com>
2023-02-11 10:17:52 +0000

    ginkgo: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I0bd3c9fde925dba8fa7dd11a7623f1a59ef57782
    Signed-off-by: Mathieu Chartier <mathieuc@google.com>
    Signed-off-by: therealmharc <therealmharc@gmail.com>
    Signed-off-by: neophyte <neophyteplayground@gmail.com>

--- device.mk
+++ device.mk
@@ -297,6 +297,9 @@ PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 28
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
