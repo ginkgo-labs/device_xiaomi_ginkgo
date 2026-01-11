63be6c94418717835a5a92cd87621f5e3d5a9417
neophyte <neophyteplayground@gmail.com>
2026-01-11 16:10:06 +0000

    ginkgo: Migrate to new DT2W node
    * Drop power AIDL extension
    * Set double_tap mode via powerhint
    
    Signed-off-by: neophyte <neophyteplayground@gmail.com>

--- device.mk
+++ device.mk
@@ -257,8 +257,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
     $(LOCAL_PATH)/configs/perf/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
-$(call soong_config_set,power_libperfmgr,mode_extension_lib,//$(LOCAL_PATH):libperfmgr-ext-xiaomi)
-
 PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel \
